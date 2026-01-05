20a34ebc08699a073a5dbbeb8d406d174faff046
Rubén Cruz <rubencfu@gmail.com>
2026-01-05 18:52:52 +0100

    fix(app): fix peer dependencies installing when they should not

--- src/infrastructure/factories/storage-adpater-factory.ts
+++ src/infrastructure/factories/storage-adpater-factory.ts
@@ -1,13 +1,14 @@
 import { StorageConfig } from '../../core/config';
+import { UnknownAny } from '../../core/types';
 
-import { AzureBlobStorageAdapter } from '../adapters/azure-blob-storage-adapter';
-import { DefaultUrlStorageAdapter } from '../adapters/url-storage-adapter';
 import { LocalStorageAdapter } from '../adapters/local-storage-adapter';
 import { HTTPStorageAdapter } from '../adapters/http-storage-adapter';
-import { GCSStorageAdapter } from '../adapters/gcs-storage-adapter';
-import { SSHStorageAdapter } from '../adapters/ssh-storage-adapter';
-import { S3StorageAdapter } from '../adapters/s3-storage-adapter';
-import { UnknownAny } from '../../core/types';
+import { DefaultUrlStorageAdapter } from '../adapters/url-storage-adapter';
+
+import type { AzureBlobStorageAdapter } from '../adapters/azure-blob-storage-adapter';
+import type { GCSStorageAdapter } from '../adapters/gcs-storage-adapter';
+import type { SSHStorageAdapter } from '../adapters/ssh-storage-adapter';
+import type { S3StorageAdapter } from '../adapters/s3-storage-adapter';
 
 export type StorageAdapterMap = {
   local: LocalStorageAdapter;
@@ -42,22 +43,66 @@ type StorageAdapterByConfigType<C extends StorageConfig> = C extends { type: 'lo
               : never;
 
 export class StorageAdapterFactory {
-  static create<C extends StorageConfig>(config: C): StorageAdapterByConfigType<C> {
+  static async create<C extends StorageConfig>(config: C): Promise<StorageAdapterByConfigType<C>> {
     switch (config.type) {
-      case 'local':
+      case 'local': {
         return new LocalStorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 'http':
+      }
+
+      case 'http': {
         return new HTTPStorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 's3':
+      }
+
+      case 's3': {
+        let S3StorageAdapter;
+        try {
+          ({ S3StorageAdapter } = await import('../adapters/s3-storage-adapter'));
+        } catch {
+          throw new Error(
+            'Missing peer dependency: @aws-sdk/client-s3 module not found. Please install it.'
+          );
+        }
         return new S3StorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 'azure':
+      }
+
+      case 'azure': {
+        let AzureBlobStorageAdapter;
+        try {
+          ({ AzureBlobStorageAdapter } = await import('../adapters/azure-blob-storage-adapter'));
+        } catch {
+          throw new Error(
+            'Missing peer dependency: "@azure/identity or @azure/storage-blob module not found. Please install them.'
+          );
+        }
         return new AzureBlobStorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 'gcs':
+      }
+
+      case 'gcs': {
+        let GCSStorageAdapter;
+        try {
+          ({ GCSStorageAdapter } = await import('../adapters/gcs-storage-adapter'));
+        } catch {
+          throw new Error(
+            'Missing peer dependency: @google-cloud/storage module not found. Please install it.'
+          );
+        }
         return new GCSStorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 'ssh':
+      }
+
+      case 'ssh': {
+        let SSHStorageAdapter;
+        try {
+          ({ SSHStorageAdapter } = await import('../adapters/ssh-storage-adapter'));
+        } catch {
+          throw new Error('Missing peer dependency: ssh2 module not found. Please install it.');
+        }
         return new SSHStorageAdapter(config) as StorageAdapterByConfigType<C>;
-      case 'url':
+      }
+
+      case 'url': {
         return new DefaultUrlStorageAdapter() as StorageAdapterByConfigType<C>;
+      }
+
       default:
         throw new Error(`Unsupported storage type: ${(config as UnknownAny).type}`);
     }
